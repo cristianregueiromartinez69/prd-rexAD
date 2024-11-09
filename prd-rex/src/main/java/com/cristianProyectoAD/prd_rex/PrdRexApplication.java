56d0e28a1be9b4c594b15b5a1e45ac047340186d
cristian <cregueiromartinez@danielcastelao.org>
2024-11-09 14:50:55 +0100

    commit inicial para el segundo microservicio prd-rex

---
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/PrdRexApplication.java
@@ -0,0 +1,14 @@
+package com.cristianProyectoAD.prd_rex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class PrdRexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrdRexApplication.class, args);
+	}
+
+}
---
+++ prd-rex/src/test/java/com/cristianProyectoAD/prd_rex/PrdRexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianProyectoAD.prd_rex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrdRexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
