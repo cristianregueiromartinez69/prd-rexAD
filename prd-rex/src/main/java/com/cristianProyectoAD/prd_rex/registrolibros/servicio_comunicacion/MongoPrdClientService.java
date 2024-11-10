65ec6e746a07545edf6fc0d92ce3557b73d3925f
cristian <cregueiromartinez@danielcastelao.org>
2024-11-10 16:46:21 +0100

    documentacion servicio comunicacion con mongo

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/registrolibros/servicio_comunicacion/MongoPrdClientService.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/registrolibros/servicio_comunicacion/MongoPrdClientService.java
@@ -6,9 +6,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/**
+ * Servicio de comunicacion con el microservicio de mongo para mandar registro de libros
+ * @author cristian
+ * @version 1.0
+ */
 @FeignClient(name = "nonrelational-prd-query", url="http://localhost:8083")
 public interface MongoPrdClientService {
 
+    /**
+     * Metodo para mandar el libro a la base de datos de mongo
+     * @param libroDTO el libro
+     * @return un mensage indicando si se envió correctamente o no
+     */
     @PostMapping("/nonrelational-prd-query/libros/guardar")
     ResponseEntity<String> saveBooksMongo(@RequestBody LibroDTO libroDTO);
 }
