8a00579fa9d223062bd138dd658bf8bc8f9f6a4b
Cristian <cregueiromartinez@danielcastelao.org>
2024-11-11 09:26:20 +0100

    documentacion  de registro de libros

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/registrolibros/controlador/RestControllerRegistroLibros.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/registrolibros/controlador/RestControllerRegistroLibros.java
@@ -8,16 +8,31 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Restcontroller del registro de libros de prd-rex
+ * @author cristian
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/prd-rex/libros")
 public class RestControllerRegistroLibros {
 
+    //variable del servicio de libros
     private final PrdRexLibroService prdRexLibroService;
 
+    /**
+     * Constructor de la clase
+     * @param prdRexLibroService el servicio prd-rex
+     */
     public RestControllerRegistroLibros(PrdRexLibroService prdRexLibroService) {
         this.prdRexLibroService = prdRexLibroService;
     }
 
+    /**
+     * Metodo para mandar los datos que van a venir de con-external
+     * @param libroDTO el libro
+     * @return el registro en las bases de datos
+     */
     @PostMapping("/registro")
     public ResponseEntity<String> registroLibrosBasesDatos(@RequestBody LibroDTO libroDTO) {
         return prdRexLibroService.registroLibrosBases(libroDTO);
