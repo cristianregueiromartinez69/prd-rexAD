47ed78ade405d8c99866e422e2ebfaa52e51ee81
cristian <cregueiromartinez@danielcastelao.org>
2024-11-12 19:46:50 +0100

    cambios en servicio de consulta de libros por isbn

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/isbn/postgresSQL/servicio/LibroIsbnService.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/isbn/postgresSQL/servicio/LibroIsbnService.java
@@ -2,6 +2,7 @@ package com.cristianProyectoAD.prd_rex.consultas.isbn.postgresSQL.servicio;
 
 import com.cristianProyectoAD.prd_rex.registrolibros.dto.LibroDTO;
 import com.cristianProyectoAD.prd_rex.registrolibros.servicio_comunicacion.PostgresSQLPrdClientService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +19,9 @@ public class LibroIsbnService {
     public ResponseEntity<LibroDTO> getLibroByIsbn(@RequestParam String isbn) {
         ResponseEntity<LibroDTO> responsePostgres = postgresSQLPrdClientService.getLibroByIsbn(isbn);
 
-        if(responsePostgres.getStatusCode().is2xxSuccessful()) {
-            return ResponseEntity.ok(responsePostgres.getBody());
+        if(responsePostgres.getStatusCode() == HttpStatus.NOT_FOUND) {
+            return new ResponseEntity<>(responsePostgres.getBody(), HttpStatus.NOT_FOUND);
         }
-        return ResponseEntity.notFound().build();
+        return responsePostgres;
     }
 }
