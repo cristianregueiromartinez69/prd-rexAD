cc483c3c6c38f70213486540cf2a9ad6cbdb2f65
cristian <cregueiromartinez@danielcastelao.org>
2024-11-12 18:49:29 +0100

    metodo para mandar consulta a postgresSQL

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/registrolibros/servicio_comunicacion/PostgresSQLPrdClientService.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/registrolibros/servicio_comunicacion/PostgresSQLPrdClientService.java
@@ -3,8 +3,8 @@ package com.cristianProyectoAD.prd_rex.registrolibros.servicio_comunicacion;
 import com.cristianProyectoAD.prd_rex.registrolibros.dto.LibroDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+
 /**
  * Servicio de comunicacion con el microservicio de postgresSQL para mandar registro de libros
  * @author cristian
@@ -19,4 +19,7 @@ public interface PostgresSQLPrdClientService {
      */
     @PostMapping("/relational-prd-query/libros/guardar")
     ResponseEntity<String> saveBooksPostgres(@RequestBody LibroDTO libroDTO);
+
+    @GetMapping("/relational-prd-query/libros/consultas/isbn{isbn}")
+    ResponseEntity<LibroDTO> getLibroByIsbn(@PathVariable("isbn") String isbn);
 }
