a941fb0c6e09631a3480a750f6f3f17860fa0b9f
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 19:58:15 +0100

    estructura y metodo a cliente mongo para consulta d elibro por nombre

---
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/nombre/mongo/controlador/LibroNombreMongoRestController.java
@@ -0,0 +1,4 @@
+package com.cristianProyectoAD.prd_rex.consultas.nombre.mongo.controlador;
+
+public class LibroNombreMongoRestController {
+}
---
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/nombre/mongo/servicio/LibroNombreServiceMongo.java
@@ -0,0 +1,7 @@
+package com.cristianProyectoAD.prd_rex.consultas.nombre.mongo.servicio;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibroNombreServiceMongo {
+}
