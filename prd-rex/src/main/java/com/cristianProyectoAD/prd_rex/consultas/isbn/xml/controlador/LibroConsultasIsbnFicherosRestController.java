a7da16f60bb0141cf61b490e6e8fb44115a50d5d
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 15:58:33 +0100

    clases de servicio y controlador para buscar libro por isbn

---
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/isbn/xml/controlador/LibroConsultasIsbnFicherosRestController.java
@@ -0,0 +1,4 @@
+package com.cristianProyectoAD.prd_rex.consultas.isbn.xml.controlador;
+
+public class LibroConsultasIsbnFicherosRestController {
+}
---
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/isbn/xml/servicio/LibroIsbnServiceFicheros.java
@@ -0,0 +1,7 @@
+package com.cristianProyectoAD.prd_rex.consultas.isbn.xml.servicio;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class LibroIsbnServiceFicheros {
+}
