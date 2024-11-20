3d64344288d718f1f4e831803a07ed4d31b61dd6
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 18:40:47 +0100

    creando restController de consulta de libros en mongo por autor

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/autor/mongo/servicio/LibroAutorServiceMongo.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/autor/mongo/servicio/LibroAutorServiceMongo.java
@@ -2,7 +2,6 @@ package com.cristianProyectoAD.prd_rex.consultas.autor.mongo.servicio;
 
 import com.cristianProyectoAD.prd_rex.registrolibros.dto.LibroDTO;
 import com.cristianProyectoAD.prd_rex.servicio_comunicacion.MongoPrdClientService;
-import com.cristianProyectoAD.prd_rex.servicio_comunicacion.PostgresSQLPrdClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +15,14 @@ public class LibroAutorServiceMongo {
     /**
      * Constructor del servicio.
      *
-     * @param mongoPrdClientService Cliente que maneja la comunicación con la base de datos PostgreSQL.
+     * @param mongoPrdClientService Cliente que maneja la comunicación con la base de datos mongoDB.
      */
     public LibroAutorServiceMongo(MongoPrdClientService mongoPrdClientService) {
         this.mongoPrdClientService = mongoPrdClientService;
     }
 
     /**
-     * Consulta libros en PostgreSQL utilizando el nombre de un autor.
+     * Consulta libros en mongoDB utilizando el nombre de un autor.
      *
      * @param autor El nombre del autor.
      * @return ResponseEntity con la lista de libros del autor si se encuentran.
