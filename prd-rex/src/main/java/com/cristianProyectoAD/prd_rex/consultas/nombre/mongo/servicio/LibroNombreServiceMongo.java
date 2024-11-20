db4cd5384e6347a5b75ba3a226ac78de797ec05e
cristian <cregueiromartinez@danielcastelao.org>
2024-11-20 19:59:25 +0100

    creando servicio de mongo para consulta de libro por nombre

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/nombre/mongo/servicio/LibroNombreServiceMongo.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/nombre/mongo/servicio/LibroNombreServiceMongo.java
@@ -1,7 +1,22 @@
 package com.cristianProyectoAD.prd_rex.consultas.nombre.mongo.servicio;
 
+import com.cristianProyectoAD.prd_rex.registrolibros.dto.LibroDTO;
+import com.cristianProyectoAD.prd_rex.servicio_comunicacion.MongoPrdClientService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class LibroNombreServiceMongo {
+
+    private final MongoPrdClientService mongoPrdClientService;
+
+    public LibroNombreServiceMongo(MongoPrdClientService mongoPrdClientService) {
+        this.mongoPrdClientService = mongoPrdClientService;
+    }
+
+    public ResponseEntity<List<LibroDTO>> getLibroNombreMongo(String nombre) {
+        return mongoPrdClientService.getLibroByNombre(nombre);
+    }
 }
