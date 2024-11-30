eb787f720f6dad3be6e7269c4e7264ac0400661d
cristian <cregueiromartinez@danielcastelao.org>
2024-11-30 16:09:07 +0100

    rest controller consulta fecha registro mongo

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/fecharegistro/mongo/controlador/LibroFechaRegistroMongoRestController.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/fecharegistro/mongo/controlador/LibroFechaRegistroMongoRestController.java
@@ -1,11 +1,28 @@
 package com.cristianProyectoAD.prd_rex.consultas.fecharegistro.mongo.controlador;
 
+import com.cristianProyectoAD.prd_rex.consultas.fecharegistro.mongo.servicio.LibroFechaRegistroServicioMongo;
+import com.cristianProyectoAD.prd_rex.registrolibros.dto.LibroDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/prd-rex/libros/consulta/mongoDB")
 public class LibroFechaRegistroMongoRestController {
 
+    private final LibroFechaRegistroServicioMongo libroFechaRegistroServicio;
+
+    public LibroFechaRegistroMongoRestController(LibroFechaRegistroServicioMongo libroFechaRegistroServicio) {
+        this.libroFechaRegistroServicio = libroFechaRegistroServicio;
+    }
+
+    @GetMapping("/fecharegistro")
+    public ResponseEntity<List<LibroDTO>> getLibrosMongoFechaRegistro(LocalDate fechaInicio, LocalDate fechaFin) {
+        return libroFechaRegistroServicio.getLibrosByFechaRMongo(fechaInicio, fechaFin);
+    }
 
 }
