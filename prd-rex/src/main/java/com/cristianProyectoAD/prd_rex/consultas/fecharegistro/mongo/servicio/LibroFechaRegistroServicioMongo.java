16658368cd42df788d8d7f9993fd9c81be5e7ff7
cristian <cregueiromartinez@danielcastelao.org>
2024-11-30 16:06:38 +0100

    servicio consulta fecha registro mongo

--- prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/fecharegistro/mongo/servicio/LibroFechaRegistroServicioMongo.java
+++ prd-rex/src/main/java/com/cristianProyectoAD/prd_rex/consultas/fecharegistro/mongo/servicio/LibroFechaRegistroServicioMongo.java
@@ -1,4 +1,24 @@
 package com.cristianProyectoAD.prd_rex.consultas.fecharegistro.mongo.servicio;
 
+import com.cristianProyectoAD.prd_rex.registrolibros.dto.LibroDTO;
+import com.cristianProyectoAD.prd_rex.servicio_comunicacion.MongoPrdClientService;
+import org.springframework.cglib.core.Local;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
 public class LibroFechaRegistroServicioMongo {
+
+    private final MongoPrdClientService mongoPrdClientService;
+
+    public LibroFechaRegistroServicioMongo(MongoPrdClientService mongoPrdClientService) {
+        this.mongoPrdClientService = mongoPrdClientService;
+    }
+
+    public ResponseEntity<List<LibroDTO>> getLibrosByFechaRMongo(LocalDate fechaInicio, LocalDate fechaFin) {
+        return mongoPrdClientService.getLibroByFechaRegistro(fechaInicio, fechaFin);
+    }
 }
